7577b06ce3f7d8efa1d7a6ba4f98f9fb0f4e4c96
Kenneth Philip Ajit <143505193+KenBoi26@users.noreply.github.com>
2025-07-18 10:04:06 +0530

    Time: 2 ms (82.59%) | Memory: 15.9 MB (29.03%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
-    stack<string> st;
     vector<string> result;
 
-    void backtrack(int open_count, int closed_count, int n){
-        if(open_count == closed_count && open_count == n){
-            string temp="";
-            stack<string> temp_stack = st;
-
-            while(!temp_stack.empty()){
-                temp = temp_stack.top() + temp;
-                temp_stack.pop();
-            }
-
-            result.push_back(temp);
+    void backtrack(string current, int open_count, int closed_count, int n){
+        if(current.length() == 2*n){
+            result.push_back(current);
+            return;
         }
 
         if(open_count < n){
-            st.push("(");
-            backtrack(open_count+1, closed_count, n);
-            st.pop();
+            backtrack(current+"(", open_count+1, closed_count, n);
         }
 
         if(closed_count < open_count){
-            st.push(")");
-            backtrack(open_count, closed_count+1, n);
-            st.pop();
+            backtrack(current+")", open_count, closed_count+1,n);
         }
     }
 
     vector<string> generateParenthesis(int n) {
-        backtrack(0,0,n);
+        backtrack("",0,0,n);
         return result;
     }
 };
