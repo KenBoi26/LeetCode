7920f39903e6b250b74f8ee07ad633a33baa1b38
Kenneth Philip Ajit <143505193+KenBoi26@users.noreply.github.com>
2025-07-25 10:13:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (45.37%) - LeetSync

--- 390-elimination-game/elimination-game.cpp
+++ 390-elimination-game/elimination-game.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
-    int helper(int n, bool direction){
-        if(n==1) return 1;
-
-        if(n%2 == 1 || direction){
-            return 2*helper(n/2, !direction);
-        }else{
-            return 2*helper(n/2, !direction) - 1;
-        }
-    }
-
     int lastRemaining(int n) {
-        return helper(n, true);
+        int currL = n;
+        int diff = 1;
+        int l =1;
+        int r = n;
+        bool lefttoright  = true;
+        while(l<r){
+            if(currL%2==1){
+                l  = l+diff;
+                r= r-diff;
+            }
+            else{
+                if(lefttoright){
+                    l = l+diff;
+                }
+                else{
+                    r = r-diff;
+                }
+            }
+            diff*=2;
+            currL/=2;
+            lefttoright= !lefttoright;
+        }
+        return l;
     }
 };
