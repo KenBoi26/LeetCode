7526bd205c4ca46b523de3bc083262d1e45cb843
Kenneth Philip Ajit <143505193+KenBoi26@users.noreply.github.com>
2026-01-27 13:18:53 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (44.35%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -7,8 +7,12 @@ public:
         while(start <= end){
             int mid = (start+end)/2;
             if(nums[mid] == target) return mid;
-            else if(nums[mid] > target) end--;
-            else start++;
+            else if(nums[mid] > target){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
         }
 
         return -1;
