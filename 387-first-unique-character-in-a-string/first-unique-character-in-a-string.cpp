41cd63e5e4ea951d887984041143db414a5f764a
Kenneth Philip Ajit <143505193+KenBoi26@users.noreply.github.com>
2026-01-30 12:24:53 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (86.87%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -5,11 +5,9 @@ public:
         for(auto i:s){
             freq[i-'a']++;
         }
-
         for(int i=0; i<s.length(); i++){
             if(freq[s[i]-'a'] == 1) return i;
         }
-
         return -1;
     }
 };
