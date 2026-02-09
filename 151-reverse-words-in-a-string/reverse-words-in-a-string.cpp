626d271b658a44f7cd95ecc8e775f03a31da51ac
Kenneth Philip Ajit <143505193+KenBoi26@users.noreply.github.com>
2026-02-09 16:22:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (41.21%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
+
     string reverseWords(string s) {
-        int r = s.length()-1;
-        string output = "";
-
-        while(r>=0){
-            string temp = "";
-            while(r>=0 && s[r] == ' ') r--;
-
-            if(r<0) break;
-            
-            while(s[r] != ' '){
-                temp = s[r] + temp;
-                r--;
-                if(r < 0) break;
+       stack<string> bank;
+       string temp = "";
+       for(char i:s){
+            if(i == ' '){
+                if (!temp.empty()) {
+                    bank.push(temp);
+                    temp = "";
+                }
+            }else{
+                temp += i;
             }
-            r--;
-            
-            
-            if(!output.empty())output += ' ' ;
-            output += temp;
-        }
+       }
+       if(!temp.empty()) bank.push(temp);
+       
+       string finale = "";
+       while(bank.size() > 1){
+            finale += bank.top() + " ";
+            bank.pop();
+       }
+       finale += bank.top();
+       bank.pop();
 
-        return output;
+       return finale;
+
     }
 };
