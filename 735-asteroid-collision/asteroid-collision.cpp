6d35b0385660d9633ad3c4062c75b0534d566064
Kenneth Philip Ajit <143505193+KenBoi26@users.noreply.github.com>
2026-02-12 12:55:46 +0530

    Time: 3 ms (28.81%) | Memory: 22.2 MB (18.58%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -2,39 +2,38 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int> st;
-
         for(auto i:asteroids){
-            if(st.empty() || i > 0 || st.top()<0){
+            if(i>0){
                 st.push(i);
-            }else{
-                bool flag = false;
-                while(!st.empty() && st.top() > 0 && i<0){
-                    if(st.top() < abs(i)){
+            }
+            
+            if(i<0){
+                bool destroyed = false;
+                while(!st.empty() && st.top() > 0){
+                    if(st.top() < -i){
                         st.pop();
-                    }else if(st.top() == abs(i)){
+                    }
+                    else if(st.top() == -i){
                         st.pop();
-                        flag = true;
+                        destroyed = true;
                         break;
                     }else{
-                        flag = true;
+                        destroyed = true;
                         break;
                     }
                 }
-
-                if(!flag){
+                if(!destroyed){
                     st.push(i);
                 }
             }
         }
-
-        vector<int> v;
+        if(st.empty()) return {};
+        vector<int> ans;
         while(!st.empty()){
-            v.push_back(st.top());
+            ans.push_back(st.top());
             st.pop();
         }
-        reverse(v.begin(), v.end());
-        return v;
-
-
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
