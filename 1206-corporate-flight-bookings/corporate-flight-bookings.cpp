db03f64674737ba7e54a28e2b71c595a0fba3510
Kenneth Philip Ajit <143505193+KenBoi26@users.noreply.github.com>
2026-06-22 15:35:10 +0530

    Time: 283 ms (16.34%) | Memory: 72.7 MB (57.94%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<int> ans(n,0);
+        vector<int> ans(n+2,0);
         
         for(auto &i:bookings){
-            for(int j=i[0]-1; j<i[1]; j++){
-                ans[j] += i[2];
-            }
+            ans[i[0]] += i[2];
+            ans[i[1]+1] -= i[2];
         }
 
-        return ans;
+        for(auto i:ans) cout << i << " ";
+
+        vector<int> prefix(n);
+        prefix[0] = ans[1];
+        for(int i=1; i<n; i++){
+            prefix[i] = prefix[i-1] + ans[i+1];
+        }
+        
+
+        return prefix;
     }
 };
