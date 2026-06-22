2d3ffe4f75bfeca3d12f6c28eb8d0a960dfeccbb
Kenneth Philip Ajit <143505193+KenBoi26@users.noreply.github.com>
2026-06-22 14:56:44 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (6.40%) - LeetSync

--- 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int minStartValue(vector<int>& nums) {
-        int it=1;
-        while(true){
-            int start = it;
-            bool flag = true;
-            for(auto i:nums){
-                start += i;
-                if(start < 1){
-                    flag = false;
-                    break;
-                }
-            }
+        vector<int> prefix_sum(nums.size()+1);
 
-            if(flag) return it;
+        int min = 1;
 
-            it++;
+        for(int i=0;i<nums.size();i++){
+            prefix_sum[i+1] = prefix_sum[i] + nums[i];
+            if(min > prefix_sum[i]) min = prefix_sum[i];
         }
+        if(min > prefix_sum[nums.size()]) min = prefix_sum[nums.size()];
 
-        return -1;
+
+        if(min < 1) return min*-1 + 1;
+        return min;
     }
 };
