d2e119f57b50c92bc3b99f19b44db8aefdb998ea
Kenneth Philip Ajit <143505193+KenBoi26@users.noreply.github.com>
2026-06-23 14:41:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (49.13%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long left = 0;
+        long long right = x;
+
+        while(left < right){
+            long long mid = left+(right-left)/2;
+
+            if(mid*mid >= x) right = mid;
+            else left = mid+1;
+        }
+
+        if(right*right > x) return right-1;
+        return right;
+    }
+};
