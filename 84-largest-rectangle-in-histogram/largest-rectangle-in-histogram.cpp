f015996a285b4ea7281e661a436833e6d6f68c37
Kenneth Philip Ajit <143505193+KenBoi26@users.noreply.github.com>
2026-06-30 15:28:02 +0530

    Time: 0 ms (100.00%) | Memory: 81.7 MB (56.08%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,4 +1,58 @@
 auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
+#pragma GCC optimize("Ofast,unroll-loops,fast-math")
+#pragma GCC target("avx,avx2,fma,sse4,popcnt,lzcnt,bmi,bmi2")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+static constexpr size_t mxal = alignof(max_align_t);
+alignas(mxal) static unsigned char buf[128 * 1024 * 1024];
+static size_t pos = 0;
+
+void* operator new(size_t sz) {
+    size_t pad = (mxal - (pos % mxal)) % mxal;
+    pos += pad + sz;
+    return (void*)(&buf[pos - sz]);
+}
+
+void* operator new[](size_t sz) {
+    return operator new(sz);
+}
+
+void operator delete(void*) noexcept {}
+void operator delete[](void*) noexcept {}
+void operator delete(void*, size_t) noexcept {}
+void operator delete[](void*, size_t) noexcept {}
+
+struct chash {
+    static unsigned long long splitmix64(unsigned long long x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(unsigned long long x) const {
+        static const unsigned long long rnd =
+            chrono::steady_clock::now().time_since_epoch().count();
+
+        return splitmix64(x + rnd);
+    }
+};
+
+template <class K, class V>
+using umap = unordered_map<K, V, chash>;
+
+template <class T>
+using uset = unordered_set<T, chash>;
+
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
