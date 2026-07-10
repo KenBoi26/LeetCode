3f71e761bb23e36b8ac9e641cd38a66de653104d
Kenneth Philip Ajit <143505193+KenBoi26@users.noreply.github.com>
2026-07-10 10:43:10 +0530

    Time: 27 ms (6.81%) | Memory: 74.6 MB (6.05%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,27 +9,33 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
-
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        if(preorder.empty() || inorder.empty()) return nullptr;
+        if(preorder.empty()) return nullptr;
 
         TreeNode* root = new TreeNode(preorder[0]);
-        auto mid = find(inorder.begin(), inorder.end(), preorder[0]) - inorder.begin();
-
-        vector<int> left_preorder(preorder.begin()+1, preorder.begin() + 1 + mid);
-        vector<int> left_inorder(inorder.begin(), inorder.begin() + mid);
-
-        vector<int> right_preorder(preorder.begin()+1+mid, preorder.end());
-        vector<int> right_inorder(inorder.begin()+mid+1, inorder.end());
-
-        root->left = buildTree(left_preorder, left_inorder);
-        root->right = buildTree(right_preorder, right_inorder);
-        return root;
         
+        int index_of_root = -1;
+        for(int i=0; i<preorder.size(); i++){
+            if(inorder[i]==preorder[0]){
+                index_of_root = i;
+                break;
+            }
+        }
 
+        vector<int> left_preorder(preorder.begin()+1, preorder.begin()+index_of_root+1);
+        vector<int> left_inorder(inorder.begin(), inorder.begin()+index_of_root);
+        
+        vector<int> right_preorder(preorder.begin()+index_of_root+1, preorder.end());
+        vector<int> right_inorder(inorder.begin()+index_of_root+1, inorder.end());
 
+        TreeNode* left = buildTree(left_preorder, left_inorder);
+        TreeNode* right = buildTree(right_preorder, right_inorder);
+
+        root->left = left;
+        root->right = right;
+
+        return root;
     }
 };
